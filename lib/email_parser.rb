2dccfc63e51afa1f498e184ab5f691168d1f1254
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-02 17:16:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,13 +8,12 @@ class EmailAddressParser
 
 attr_accessor :email_addresses
 
-#@@emails =[]
 def initialize(emails)
   @emails =  emails
 end
 
 def parse 
-  @emails.split(",")
+  @emails.split(", ")
  end
 
 
